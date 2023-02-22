8d8082bd1f0de5f1b1dcbc507f257f6a89b8a404
NITSAN Technologies <sanjay@nitsan.in>
2023-02-22 22:41:30 +0530

    [TASK] Updated Extension Description

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,17 @@
 <?php
 
 $EM_CONF['ns_friendlycaptcha'] = [
-    'title' => '[NITSAN]Friendly Captcha',
-    'description' => 'Integrates Friendly captcha in EXT:form
-        and via TypoScript renderer Easy on Humans, Hard on Bots',
-    'category' => 'fe',
-    'author' => 'NITSAN Technologies Pvt Ltd',
+    'title' => '[NITSAN] Friendly Captcha',
+    'description' => 'Integrate official friendlycaptcha.com (Europe\'s most leading Captcha solution) to the TYPO3 core EXT:form extension. Live Demo: https://demo.t3planet.com/t3t-extensions/friendly-captcha Premium Support: https://t3planet.com/typo3-friendly-captcha-extension',
+    'category' => 'plugin',
+    'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-11.9.99',
+            'typo3' => '8.0.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
