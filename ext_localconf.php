2f7cb8b8ef6cba6917bd862a9291fae5b37042b6
nitsan-technologies <alfaz.nitsan@gmail.com>
2026-01-27 10:38:53 +0530

    [FEATURE]Add Site Sets

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,11 @@
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
-defined('TYPO3') || die('Access denied.');
+defined('TYPO3') || die ('Access denied.');
 
 $versionNumber =  VersionNumberUtility::convertVersionStringToArray(VersionNumberUtility::getCurrentTypo3Version());
 
-if ($versionNumber <= 13) {
+if ($versionNumber['version_main'] <= 13) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/Database.xlf'][] = 'EXT:ns_friendlycaptcha/Resources/Private/Language/Backend.xlf';
 }else{
     $GLOBALS['TYPO3_CONF_VARS']['LANG']['resourceOverrides']['EXT:form/Resources/Private/Language/Database.xlf'][] = 'EXT:ns_friendlycaptcha/Resources/Private/Language/Backend.xlf';
